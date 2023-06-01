b220e063ce15cec1feb2744111ab7098804d40b0
FilippoContessa <102584268+FilippoContessa@users.noreply.github.com>
2023-06-02 00:14:06 +0200

    Graph shift factor, function definition updated
    thanks ChatGpt

--- data_analysis.cpp
+++ data_analysis.cpp
@@ -10,15 +10,14 @@ Double_t myFunction(Double_t *x, Double_t *par)
     Double_t I0 = par[0];
     Double_t D = par[1];
     Double_t d = par[2];
-    Double_t p=par[3];
     Double_t X = x[0];
 
-    const double l = 78.4 * TMath::Power(10, 4);  //micron
+    const double l = 78.4 * TMath::Power(10, 4); // micron
 
-    const double_t k = 2 * TMath::Pi() / 0.633 ; //  micron
+    const double_t k = 2 * TMath::Pi() / 0.633; //  micron
 
-    Double_t arg1 = 0.5 * k * D * TMath::Sin (X /l + p);
-    Double_t arg2 = 0.5 * k * d * TMath::Sin(X /l + p);
+    Double_t arg1 = 0.5 * k * D * TMath::Sin(X / l);
+    Double_t arg2 = 0.5 * k * d * TMath::Sin(X / l);
 
     Double_t result = I0 * TMath::Power(TMath::Cos(arg1), 2) * TMath::Power(TMath::Sin(arg2) / arg2, 2);
 
@@ -27,14 +26,14 @@ Double_t myFunction(Double_t *x, Double_t *par)
 
 Double_t myFunction2(Double_t *x, Double_t *par)
 {
-    Double_t I0 = par[0];
-    Double_t D = par[1];
-    Double_t d = par[2];
+    Double_t D = par[0];
+    Double_t d = par[1];
     Double_t X = x[0];
 
     const double l = 78.4 * TMath::Power(10, 4);
+    const double I0 = 2.5; 
 
-    const double_t k = 2 * TMath::Pi() / 0.633  ; // micron
+    const double_t k = 2 * TMath::Pi() / 0.633; // micron
 
     Double_t arg1 = 0.5 * k * D * TMath::Sin(X / l);
     Double_t arg2 = 0.5 * k * d * TMath::Sin(X / l);
@@ -72,18 +71,20 @@ void analysis()
     TH1 *hI_0 = gI_0->GetHistogram();
 
     // valore I_0
+    
     TF1 *fI_0 = new TF1("fI_0", "gaus", 0, 10000);
 
     gI_0->Fit(fI_0);
     Double_t mean = fI_0->GetParameter(0);
+    
 
     // Definizione delle funzioni, setting parametri par[0] = I0; par[1] = D ; par[2] = d
 
-    TF1 *f1 = new TF1("f1", myFunction, 60000, 68000,4);
-    f1->SetParameters(mean, 300, 100, 0.08);
+    TF1 *f1 = new TF1("f1", myFunction, -5000,5000, 3);
+    f1->SetParameters(2.5,300,100 );
 
-    TF1 *f1c = new TF1("f1c", myFunction2, 60000, 70000, 3);
-    f1c->SetParameters(10, 300, 100);
+     TF1 *f1c = new TF1("f1c", myFunction, -1000,1000, 2);
+    f1c->SetParameters(300,100);
 
     TF1 *f2 = new TF1("f2", myFunction, 0, 20000, 3);
     f2->SetParameters(10, 300, 150);
@@ -95,7 +96,17 @@ void analysis()
     f1->SetParameters(10, 500, 150);
 
     TCanvas *c = new TCanvas("canvas", "Grafico");
-    
-    g1->Fit(f1,"R");
-    g1->Draw();
+
+    int numPoints = g1->GetN();
+    double shiftValue = 63500;
+
+    for (int i = 0; i < numPoints; i++)
+    {
+        double x, y;
+        g1->GetPoint(i, x, y);
+
+        x -= shiftValue;
+
+        g1->SetPoint(i, x, y);
+    }
 }
