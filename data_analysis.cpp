b48ae52a8c33e77010f71b26e469bc3a2d2ba0e9
FilippoContessa <102584268+FilippoContessa@users.noreply.github.com>
2023-06-03 22:19:21 +0200

    Perfect shifting, hoping for Maximi to come

--- data_analysis.cpp
+++ data_analysis.cpp
@@ -31,8 +31,27 @@ Double_t myFunction2(Double_t *x, Double_t *par)
     Double_t X = x[0];
 
     const double l = 78.4 * TMath::Power(10, 4);
-    const double I0 = 2.5; 
+    const double I0 = 2.5;
 
+    const double_t k = 2 * TMath::Pi() / 0.633; // micron
+
+    Double_t arg1 = 0.5 * k * D * TMath::Sin(X / l);
+    Double_t arg2 = 0.5 * k * d * TMath::Sin(X / l);
+
+    Double_t result = I0 * TMath::Power(TMath::Cos(arg1), 2) * TMath::Power(TMath::Sin(arg2) / arg2, 2);
+
+    return result;
+}
+
+Double_t myFunction3(Double_t *x, Double_t *par)
+{
+    Double_t D = par[0];
+
+    Double_t X = x[0];
+
+    const double l = 78.4 * TMath::Power(10, 4);
+    const double I0 = 2.5;
+    const double d = 100;
     const double_t k = 2 * TMath::Pi() / 0.633; // micron
 
     Double_t arg1 = 0.5 * k * D * TMath::Sin(X / l);
@@ -70,36 +89,12 @@ void analysis()
     TGraph *gI_0 = new TGraph("I_0.txt", "%lg%lg");
     TH1 *hI_0 = gI_0->GetHistogram();
 
-    // valore I_0
+    Int_t numPoints; 
+    Int_t shiftValue; 
     
-    TF1 *fI_0 = new TF1("fI_0", "gaus", 0, 10000);
-
-    gI_0->Fit(fI_0);
-    Double_t mean = fI_0->GetParameter(0);
-    
-
-    // Definizione delle funzioni, setting parametri par[0] = I0; par[1] = D ; par[2] = d
-
-    TF1 *f1 = new TF1("f1", myFunction, -5000,5000, 3);
-    f1->SetParameters(2.5,300,100 );
-
-     TF1 *f1c = new TF1("f1c", myFunction, -1000,1000, 2);
-    f1c->SetParameters(300,100);
-
-    TF1 *f2 = new TF1("f2", myFunction, 0, 20000, 3);
-    f2->SetParameters(10, 300, 150);
-
-    TF1 *f4 = new TF1("f4", myFunction, 0, 20000, 3);
-    f4->SetParameters(10, 250, 150);
-
-    TF1 *f5 = new TF1("f5", myFunction, 0, 20000, 3);
-    f1->SetParameters(10, 500, 150);
-
-    TCanvas *c = new TCanvas("canvas", "Grafico");
-
-    int numPoints = g1->GetN();
-    double shiftValue = 63500;
-
+    // g1 shift
+    numPoints= g1->GetN();
+    shiftValue= 63400;
     for (int i = 0; i < numPoints; i++)
     {
         double x, y;
@@ -109,6 +104,111 @@ void analysis()
 
         g1->SetPoint(i, x, y);
     }
-       g1->Fit(f1, "R");
-    g1->Draw("AP")
+    // g2 shift
+    numPoints = g2->GetN();
+    shiftValue = 64200;
+    for (int i = 0; i < numPoints; i++)
+    {
+        double x, y;
+        g2->GetPoint(i, x, y);
+
+        x -= shiftValue;
+
+        g2->SetPoint(i, x, y);
+    }
+    // g4 shift
+    numPoints = g4->GetN();
+    shiftValue = 63800;
+    for (int i = 0; i < numPoints; i++)
+    {
+        double x, y;
+        g4->GetPoint(i, x, y);
+
+        x -= shiftValue;
+
+        g4->SetPoint(i, x, y);
+    }
+    // g4_100x shift
+    numPoints = g4_100x->GetN();
+    shiftValue = 63800;
+    for (int i = 0; i < numPoints; i++)
+    {
+        double x, y;
+        g4_100x->GetPoint(i, x, y);
+
+        x -= shiftValue;
+
+        g4_100x->SetPoint(i, x, y);
+    }
+    // g5 shift
+    numPoints = g5->GetN();
+    shiftValue = 63800;
+    for (int i = 0; i < numPoints; i++)
+    {
+        double x, y;
+        g5->GetPoint(i, x, y);
+
+        x -= shiftValue;
+
+        g5->SetPoint(i, x, y);
+    }
+    // g5_100x shift
+    numPoints = g5_100x->GetN();
+    shiftValue = 63700;
+    for (int i = 0; i < numPoints; i++)
+    {
+        double x, y;
+        g5_100x->GetPoint(i, x, y);
+
+        x -= shiftValue;
+
+        g5_100x->SetPoint(i, x, y);
+    }
+    // g5G shift
+    numPoints = g5G->GetN();
+    shiftValue = 62900;
+    for (int i = 0; i < numPoints; i++)
+    {
+        double x, y;
+        g5G->GetPoint(i, x, y);
+
+        x -= shiftValue;
+
+        g5G->SetPoint(i, x, y);
+    }
+    // valore I_0
+    /*
+        TF1 *fI_0 = new TF1("fI_0", "gaus", 0, 10000);
+        gI_0->Fit(fI_0);
+        Double_t mean = fI_0->GetParameter(0);
+    */
+
+    // Definizione delle funzioni, setting parametri par[0] = I0; par[1] = D ; par[2] = d
+
+    TF1 *f1 = new TF1("f1", myFunction, -4000, 4000, 3);
+    f1->SetParameters(2.5, 300, 100);
+    f1->SetParLimits(0, 2, 3);
+    f1->SetParLimits(1, 300, 301);
+    f1->SetParLimits(2, 99, 101);
+
+    TF1 *f2 = new TF1("f2", myFunction, -4500, 4500, 3);
+    f2->SetParameters(4.7, 300, 150);
+
+    TF1 *f4 = new TF1("f4", myFunction, -5000, 5000, 3);
+    f4->SetParameters(0.6, 250, 150);
+
+    TF1 *f4_100x = new TF1("f4_100x", myFunction, -5000, 5000, 3);
+    f4_100x->SetParameters(4.7, 250, 150); // verify par 1, 2
+
+    TF1 *f5 = new TF1("f5", myFunction, -2000, 2000, 3);
+    f5->SetParameters(0.5, 500, 150);
+
+    TF1 *f5G = new TF1("f5G", myFunction, -2000, 2000, 3);
+    f5G->SetParameters(0.5, 500, 150);
+
+    TF1 *f5_100x = new TF1("f5_100x", myFunction, -2000, 2000, 3);
+    f5_100x->SetParameters(4.7, 500, 150);
+
+    TCanvas *c = new TCanvas("canvas", "Grafico");
+
 }
